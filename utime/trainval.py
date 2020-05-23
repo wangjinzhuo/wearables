8ba1594b43c6ed4eb1efd8c9391187aa5f23de0d
wangjinzhuo <cr7or9@163.com>
2020-05-23 21:33:33 +0800

    utime

---
+++ utime/trainval.py
@@ -0,0 +1,149 @@
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import os
+import time
+import math
+import random
+import argparse
+
+from utils import *
+from utime import *
+
+def parse_cmd_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument("--resume", "-r", action="store_true", help="resume from checkpoint")
+    args = parser.parse_args()
+    return args
+
+args = parse_cmd_args()
+
+device      = "cuda" if torch.cuda.is_available() else "cpu"
+best_acc    = 0 # best val accuracy
+start_epoch = 0 # start from epoch 0 or last checkpoint epoch
+
+print("preparing train and validation dataloader ...")
+train_loader = torch.load('../data/mass/ss1_loader.pt')
+val_loader   = torch.load('../data/mass/ss5_loader.pt')
+
+print('middle ...')
+train_loader = make_seq_loader(train_loader, seq_len=35, stride=35)
+val_loader   = make_seq_loader(val_loader, seq_len=35, stride=35)
+print("finish preparing train and validation dataloader ...")
+
+net = Utime()
+net = net.to(device)
+if device == "cuda":
+    net = nn.DataParallel(net)
+    cudnn.benchmark = True
+
+if args.resume:
+    # load checkpoint
+    print("resuming from checkpoint")
+    assert os.path.isdir("checkpoint"), "Error: no checkpoint directory found"
+    checkpoint = torch.load("./checkpoint/ckpt.pth")
+    net.load_state_dict(checkpoint["net"])
+    best_acc = checkpoint["acc"]
+    start_epoch = checkpoint["epoch"]
+    print("best acc: ", best_acc)
+
+def gdl(pred, gt):
+    # generalized dice loss
+    # pred: 32, 5, 35
+    # gt  : 32, 35
+    onehot_y  = F.one_hot(gt.long(), 5)
+    pred_     = pred.permute(0,2,1)
+
+    intersection = torch.sum(onehot_y * pred_)
+    union        = torch.sum(onehot_y + pred_)
+    loss         = 1 - 2 * intersection / (union * 5)
+
+    predl   = torch.argmax(pred, dim=1)
+    correct = torch.sum(torch.eq(predl.long(), gt.long()))
+    total   = torch.numel(gt)
+    return loss, correct, total
+
+
+#w = torch.tensor([6227/58600, 4724/58600, 29534/58600, 7651/58600, 10464/58600]).to(device)
+#criterion = nn.CrossEntropyLoss(w)
+#criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=1e-4, momentum=0.9, weight_decay=5e-4)
+#optimizer = optim.Adam(net.parameters(), lr=5e-6, betas=(0.9, 0.999), eps=1e-8)
+
+# Training
+def train(epoch):
+    print('Train - Epoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+
+    for batch_idx, (inputs, targets) in enumerate(train_loader):
+        inputs  = inputs.to(device, dtype=torch.float)
+        targets = targets.to(device, dtype=torch.long)
+        #print(inputs.size(), targets.size()) # bs, 1, dim*35;  bs, 35
+        if inputs.size(2) != 35*3000:
+            idx    = list(range(0,6000*35,2))
+            inputs = inputs[:,:,idx]
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        #print(outputs.size()) # bs, 5,35
+        #print(targets.size()) # bs,35
+        #loss = criterion(outputs, targets)
+        loss, correct_batch, total_batch = gdl(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+        correct    += correct_batch.item()
+        total      += total_batch
+        progress_bar(batch_idx, len(train_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+# Validataion
+def val(epoch):
+    print('Val - Epoch: %d' % epoch)
+    global best_acc
+    net.eval()
+    val_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(val_loader):
+            inputs  = inputs.to(device, dtype=torch.float)
+            targets = targets.to(device, dtype=torch.long)
+            if inputs.size(2) != 35*3000:
+                idx    = list(range(0,6000*35,2))
+                inputs = inputs[:,:,idx]
+            outputs = net(inputs)
+            loss, correct_batch, total_batch = gdl(outputs, targets)
+            correct  += correct_batch.item()
+            total    += total_batch
+            val_loss += loss.item()
+            progress_bar(batch_idx, len(val_loader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (val_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        print(acc)
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.pth')
+        best_acc = acc
+
+lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.5)
+for epoch in range(start_epoch, start_epoch+400):
+    train(epoch)
+    val(epoch)
+    lr_scheduler.step()
+
+print("best acc: ", best_acc)
---
+++ utime/utils.py
@@ -0,0 +1,152 @@
+'''Some helper functions for PyTorch, including:
+    - make_dataloader: prepare DataLoader instance for trainval and test.
+    - make_seq_dataloader: prepare DataLoader instance with multiple epoches for trainval and test.
+    - get_mean_and_std: calculate the mean and std value of dataset.
+    - msr_init: net parameter initialization.
+    - progress_bar: progress bar mimic xlua.progress.
+'''
+import os
+import sys
+import time
+import math
+import random
+import numpy as np
+
+from scipy.io import loadmat
+from torch.utils.data import Dataset, TensorDataset, DataLoader
+
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+
+def make_seq_loader(loader, seq_len, stride):
+    # input:  loader of size [#n, 1, #dim], [#n]
+    # reture: loader of size [#n, seq_len, #dim], [#n]
+
+    x, y   = loader.dataset.tensors[0], loader.dataset.tensors[1]
+    idx    = gen_seq(x.shape[0], seq_len, stride)
+    xx, yy = [x[i:i+seq_len, :, :] for i in idx], [y[i:i+seq_len] for i in idx]
+    xx     = [x.reshape(-1, x.shape[0]*x.shape[2]) for x in xx]
+    xx, yy = [x.unsqueeze(0) for x in xx], [y.unsqueeze(0) for y in yy]
+    xx, yy = torch.cat(xx), torch.cat(yy)
+    dataset = TensorDataset(xx, yy)
+    loader  = DataLoader(dataset, batch_size=loader.batch_size)
+    return loader
+
+def gen_seq(n, seq_len, stride):
+    res = []
+    for i in range(0, n, stride):
+        if i + seq_len <= n:
+            res.append(i)
+    return res
+
+def get_mean_and_std(dataset):
+    '''Compute the mean and std value of dataset.'''
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
+    mean = torch.zeros(3)
+    std = torch.zeros(3)
+    print('==> Computing mean and std..')
+    for inputs, targets in dataloader:
+        for i in range(3):
+            mean[i] += inputs[:,i,:,:].mean()
+            std[i] += inputs[:,i,:,:].std()
+    mean.div_(len(dataset))
+    std.div_(len(dataset))
+    return mean, std
+
+def init_params(net):
+    '''Init layer parameters.'''
+    for m in net.modules():
+        if isinstance(m, nn.Conv2d):
+            init.kaiming_normal(m.weight, mode='fan_out')
+            if m.bias:
+                init.constant(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm2d):
+            init.constant(m.weight, 1)
+            init.constant(m.bias, 0)
+        elif isinstance(m, nn.Linear):
+            init.normal(m.weight, std=1e-3)
+            if m.bias:
+                init.constant(m.bias, 0)
+
+
+_, term_width = os.popen('stty size', 'r').read().split()
+term_width = int(term_width)
+
+TOTAL_BAR_LENGTH = 65.
+last_time = time.time()
+begin_time = last_time
+def progress_bar(current, total, msg=None):
+    global last_time, begin_time
+    if current == 0:
+        begin_time = time.time()  # Reset for new bar.
+
+    cur_len = int(TOTAL_BAR_LENGTH*current/total)
+    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+
+    sys.stdout.write(' [')
+    for i in range(cur_len):
+        sys.stdout.write('=')
+    sys.stdout.write('>')
+    for i in range(rest_len):
+        sys.stdout.write('.')
+    sys.stdout.write(']')
+
+    cur_time = time.time()
+    step_time = cur_time - last_time
+    last_time = cur_time
+    tot_time = cur_time - begin_time
+
+    L = []
+    L.append('  Step: %s' % format_time(step_time))
+    L.append(' | Tot: %s' % format_time(tot_time))
+    if msg:
+        L.append(' | ' + msg)
+
+    msg = ''.join(L)
+    sys.stdout.write(msg)
+    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
+        sys.stdout.write(' ')
+
+    # Go back to the center of the bar.
+    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
+        sys.stdout.write('\b')
+    sys.stdout.write(' %d/%d ' % (current+1, total))
+
+    if current < total-1:
+        sys.stdout.write('\r')
+    else:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+def format_time(seconds):
+    days = int(seconds / 3600/24)
+    seconds = seconds - days*3600*24
+    hours = int(seconds / 3600)
+    seconds = seconds - hours*3600
+    minutes = int(seconds / 60)
+    seconds = seconds - minutes*60
+    secondsf = int(seconds)
+    seconds = seconds - secondsf
+    millis = int(seconds*1000)
+
+    f = ''
+    i = 1
+    if days > 0:
+        f += str(days) + 'D'
+        i += 1
+    if hours > 0 and i <= 2:
+        f += str(hours) + 'h'
+        i += 1
+    if minutes > 0 and i <= 2:
+        f += str(minutes) + 'm'
+        i += 1
+    if secondsf > 0 and i <= 2:
+        f += str(secondsf) + 's'
+        i += 1
+    if millis > 0 and i <= 2:
+        f += str(millis) + 'ms'
+        i += 1
+    if f == '':
+        f = '0ms'
+    return f
