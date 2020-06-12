2e8593154a0872290a3ad0b12476552c175195d1
wangjinzhuo <cr7or9@163.com>
2020-06-12 10:09:26 +0800

    dsn revisions

--- dsn/dsn.py
+++ dsn/dsn.py
@@ -5,23 +5,23 @@ import torch.nn.functional as F
 Fs = 200
 
 class BiLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+    def __init__(self, input_size, hidden_size, num_layers):
         super(BiLSTM, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        #self.fc = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         # set initial hidden and cell states
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda() #RuntimeError: Input and hidden tensors are not at the same device
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).cuda()
+        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).cuda() #RuntimeError: Input and hidden tensors are not at the same device
+        c0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).cuda()
 
         # forward propagate LSTM
         out, _ = self.lstm(x, (h0, c0))
 
         # decode the hidden state of the last time step
-        out = self.fc(out[:, -1, :])
+        #out = self.fc(out[:, -1, :])
         return out
 
 class DeepSleepNet(nn.Module):
@@ -59,7 +59,7 @@ class DeepSleepNet(nn.Module):
             nn.MaxPool1d(kernel_size=2, stride=2),
         )
         self.features_seq = nn.Sequential(
-            BiLSTM(7296, 512, 2, 512),
+            BiLSTM(7296, 512, 2),
             nn.Dropout(),
         )
         self.res = nn.Linear(7296, 1024)
@@ -74,19 +74,16 @@ class DeepSleepNet(nn.Module):
         x_l = x_l.flatten(1,2)
         x = torch.cat((x_s, x_l),1)
         x_seq = x.unsqueeze(1)
-        print(x_seq.shape)
-        x_lstm_b = self.features_seq(x_seq)
-        print(x_lstm_b.shape)
-        x_lstm_f = self.features_seq(x_seq)
-        x_lstm = torch.cat((x_lstm_b,x_lstm_f),1)
+        x_blstm = self.features_seq(x_seq)
+        x_blstm = torch.squeeze(x_blstm, 1)
         x_res = self.res(x)
-        x = torch.mul(x_res, x_lstm)
+        x = torch.mul(x_res, x_blstm)
         x = self.classifier(x)
         return x
 
 if __name__ == '__main__':
     ch_num = 1
-    batch_size = 5
+    batch_size = 3
     net = DeepSleepNet(ch=ch_num)
     net = net.cuda()
     inputs = torch.rand(batch_size, ch_num, int(Fs*30))
