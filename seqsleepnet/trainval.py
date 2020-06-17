e33f3d5065511b29d68ff91e295e466c2f53d11b
wangjinzhuo <cr7or9@163.com>
2020-06-17 20:42:31 +0800

    ckpt

--- seqsleepnet/trainval.py
+++ seqsleepnet/trainval.py
@@ -47,7 +47,7 @@ if args.resume:
     # load checkpoint
     print("resuming from checkpoint")
     assert os.path.isdir("checkpoint"), "Error: no checkpoint directory found"
-    checkpoint = torch.load("./checkpoint/ckpt.pth")
+    checkpoint = torch.load("./checkpoint/stft_ckpt.pth")
     net.load_state_dict(checkpoint["net"])
     best_acc = checkpoint["acc"]
     start_epoch = checkpoint["epoch"]
